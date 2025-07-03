ddee00d66f8f1e940b0544c0606ccb2fea57876c
Jirayu Kaewsing <46487993+StrixzIV@users.noreply.github.com>
2025-07-03 19:22:07 +0000

    Change file

--- ex00.c
+++ ex00.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello, world!");
+
+    int number;
+    char *buffer = malloc(1000);
+
+    printf("Type something: ");
+    scanf("%s", buffer);
+    
+    printf("Number: ");
+    scanf("%d", &number);
+
+    printf("String: \"%s\", Number = %d\n", buffer, number);
+    
+    free(buffer);
+
 }
